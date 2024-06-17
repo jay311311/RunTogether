0e5ac8ea9a05141b7ec0bf8d627d5dd102bfd9f2
jooeun kim <jay311311@gmail.com>
2024-06-17 13:18:38 -0400

    Initial Commit

---
+++ RunTogetherUITests/RunTogetherUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RunTogetherUITestsLaunchTests.swift
+//  RunTogetherUITests
+//
+//  Created by Jooeun Kim on 2024-06-17.
+//
+
+import XCTest
+
+final class RunTogetherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
